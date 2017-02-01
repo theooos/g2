ed0fc952f0a5de4191e36ad6a6bc142f0cb68752
Peran Truscott <perantruscott@gmail.com>
2017-02-01 02:00:46 +0000

    added skeleton classes for players, zombies (theo wins, bots is just too confusing), AIPlayers, and a really basic class for weapon

---
+++ src/server/game/AIPlayer.java
@@ -0,0 +1,21 @@
+package server.game;
+
+/**
+ * Created by peran on 01/02/17.
+ */
+public class AIPlayer extends Player {
+
+    public AIPlayer(Vector2 pos, Vector2 dir, int team, int phase, Weapon w1, Weapon w2) {
+        super(pos, dir, team, phase, w1, w2);
+    }
+
+    public void live() {
+        move();
+        //any other methods the ai may do once a tick
+    }
+
+    protected void move() {
+        //movement based on more advanced ai code
+    }
+
+}
