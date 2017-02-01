d87f7ad7c565394ad6fc7d21e32ab1570e00c7ca
Peran Truscott <perantruscott@gmail.com>
2017-02-01 02:00:46 +0000

    added skeleton classes for players, zombies (theo wins, bots is just too confusing), AIPlayers, and a really basic class for weapon

---
+++ src/server/game/Weapon.java
@@ -0,0 +1,15 @@
+package server.game;
+
+/**
+ * Created by peran on 01/02/17.
+ */
+public class Weapon {
+
+    /**
+     * skeleton class to represent future weapons
+     * will probably use inheritence
+     */
+    public Weapon() {
+
+    }
+}
