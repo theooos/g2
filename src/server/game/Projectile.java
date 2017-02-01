b94683336ed6e06b6baaf28ea935368bda2192b0
Peran Truscott <perantruscott@gmail.com>
2017-02-01 01:15:14 +0000

    added a couple of funky projectile types for an example of how easy cool stuff is with inheritence

--- src/server/game/Projectile.java
+++ src/server/game/Projectile.java
@@ -9,10 +9,22 @@ public class Projectile extends MovableEntity {
     protected int lifespan;
     protected int radius;
 
+
+    /**
+     * A boring default projectile
+     * @param damage the damage at birth
+     * @param lifespan the lifespan before the prj dies
+     * @param radius the radius of the prj at the start
+     * @param pos the starting pos of the prj
+     * @param dir the starting direction of the prj
+     * @param speed the speed of the prj
+     * @param phase the phase the prj is in
+     */
     public Projectile(int damage, int lifespan, int radius, Vector2 pos, Vector2 dir, float speed, int phase) {
         this.damage = damage;
         this.lifespan = lifespan;
         this.damagable = false;
+        this.visible = true;
         this.radius = radius;
         this.pos = pos;
         this.dir = dir;
@@ -34,6 +46,13 @@ public class Projectile extends MovableEntity {
         if (lifespan < 1) {
             setHealth(0);
         }
+    }
+
+    /**
+     * sets the lifespan of a proj to 0
+     */
+    public void kill() {
+        lifespan = 0;
     }
 
     public int getRadius() {
