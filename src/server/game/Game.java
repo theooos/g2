445da86dcf7457035d3526088dc86de1c8947071
Peran Truscott <perantruscott@gmail.com>
2017-02-03 03:37:04 +0000

    added a couple of ghost methods to weapon and game

--- src/server/game/Game.java
+++ src/server/game/Game.java
@@ -1,5 +1,9 @@
 package server.game;
 
+import networking.Connection;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -7,11 +11,21 @@ import java.util.TimerTask;
  * Created by peran on 27/01/17.
  */
 public class Game {
-    Timer t;
-    int countdown;
-    int tick = 60;
+    private Timer t;
+    private int countdown;
+    private int tick = 60;
+    private int map;
+    private ArrayList<Connection> playerConnections;
+    private int numPlayers;
 
-    public Game() {
+
+    public Game(ArrayList<Connection> playerConnections, int numPlayers, int map) {
+        this.map = map;
+        this.playerConnections = playerConnections;
+        this.numPlayers = numPlayers;
+
+        loadMap(map);
+
         t = new Timer();
         countdown = 10*60*tick; //ten minutes
 
@@ -33,5 +47,25 @@ public class Game {
     public void endGame() {
         t.cancel();
         t.purge();
+    }
+
+    public void loadMap(int map) {
+
+    }
+
+    /**
+     * creates the player and places it on one of the inital spawn points on the loaded map
+     * @param p the player
+     */
+    public void initalPlayerSpawn(Player p) {
+
+    }
+
+    /**
+     * respawns a bot or player in a valid location
+     * @param e the entity to be respawned
+     */
+    public void respawn(MovableEntity e) {
+
     }
 }
--- src/server/game/Weapon.java
+++ src/server/game/Weapon.java
@@ -4,12 +4,37 @@ package server.game;
  * Created by peran on 01/02/17.
  */
 public class Weapon {
+    protected float accuracy;
+    protected float recoil;
+    protected float recoilRecovery;
+    protected int magSize;
+    protected int heatPerShot;
+    protected int currentHeat;
+    protected int coolDownRate;
+    protected Projectile shotType;
 
     /**
      * skeleton class to represent future weapons
      * will probably use inheritence
      */
     public Weapon() {
-
+        magSize = 100;
+        currentHeat = 0;
     }
+
+    public void live() {
+        if (currentHeat > coolDownRate) {
+            currentHeat -= coolDownRate;
+        } else if (currentHeat > 0) {
+            currentHeat = 0;
+        }
+    }
+
+    public void fire(Vector2 pos, Vector2 dir) {
+        if (magSize-currentHeat < heatPerShot) return;
+        currentHeat += heatPerShot;
+        //create projectiles or sommit
+    }
+
+
 }
