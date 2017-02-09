f272fe8d4e350505d3feabebe21f9a13ede914c2
Bianca <bab581@student.bham.ac.uk>
2017-02-09 14:45:59 +0000

    Added user log in to the lobby

--- src/client/ui/MainMenu.java
+++ src/client/ui/MainMenu.java
@@ -1,7 +1,40 @@
 package client.ui;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by bianca on 06/02/2017.
  */
-public class MainMenu {
+class MainMenu extends JPanel {
+    private JButton start;
+    private JButton help;
+    private JButton about;
+    private JPanel menu;
+
+    public MainMenu(JPanel content) {
+
+        this.menu = content;
+        setOpaque(true);
+        //setBackground(Color.RED.darker().darker());
+
+        start = new JButton("Start");
+        help = new JButton("Help");
+        about = new JButton("About");
+
+        start.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cardLayout = (CardLayout) menu.getLayout();
+                cardLayout.next(menu);
+            }
+        });
+
+        add(start, BorderLayout.CENTER);
+        add(help, BorderLayout.CENTER);
+        add(about, BorderLayout.CENTER);
+    }
 }
+
