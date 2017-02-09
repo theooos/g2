1a66ef264ba5c27c947c065d8db969f203152662
Bianca <bab581@student.bham.ac.uk>
2017-02-09 14:45:59 +0000

    Added user log in to the lobby

--- src/client/ui/Display.java
+++ src/client/ui/Display.java
@@ -3,7 +3,6 @@ package client.ui;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 
 
 /**
@@ -11,9 +10,9 @@ import java.awt.event.*;
  */
 public class Display {
 
-    private JPanel content;
     private MainMenu menu;
-    private Lobby lobby;
+    private LobbyUI lobby;
+    private JPanel content;
 
     public void displayUI() {
 
@@ -24,85 +23,15 @@ public class Display {
         content.setLayout(new CardLayout());
 
         menu = new MainMenu(content);
-        lobby = new Lobby(content);
+        lobby = new LobbyUI(content);
 
         content.add(menu, "Panel 1");
         content.add(lobby, "Panel 2");
 
         frame.setSize(700, 500);
         frame.setContentPane(content);
-        frame.pack();
-        frame.setLocationByPlatform(true);
+        //frame.pack();
+        //frame.setLocationByPlatform(true);
         frame.setVisible(true);
     }
 }
-
-    class MainMenu extends JPanel
-    {
-        private JButton start;
-        private JButton help;
-        private JButton about;
-        private JPanel menu;
-
-        public MainMenu(JPanel content) {
-
-            this.menu = content;
-            //setBackground(Color.RED.darker().darker());
-
-            start = new JButton("Start");
-            help = new JButton("Help");
-            about = new JButton("About");
-
-            start.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    CardLayout cardLayout = (CardLayout) menu.getLayout();
-                    cardLayout.next(menu);
-                }
-            });
-
-            add(start, BorderLayout.CENTER);
-            add(help, BorderLayout.CENTER);
-            add(about, BorderLayout.CENTER);
-        }
-
-        @Override
-        public Dimension getPreferredSize()
-        {
-            return (new Dimension(500, 500));
-        }
-    }
-
-    class Lobby extends JPanel
-    {
-        private JTextField names;
-        private JButton cancel;
-        private JPanel lobby;
-
-        public Lobby(JPanel content){
-
-            this.lobby = content;
-            setOpaque(true);
-
-            JButton back = new JButton("Back");
-            JTextField text = new JTextField();
-            text.setText("Lol");
-
-            back.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    CardLayout cardLayout = (CardLayout) lobby.getLayout();
-                    cardLayout.next(lobby);
-                }
-            });
-
-            add(text, BorderLayout.CENTER);
-            add(back, BorderLayout.CENTER);
-        }
-
-        @Override
-        public Dimension getPreferredSize()
-        {
-            return (new Dimension(500, 500));
-        }
-    }
--- src/client/ui/LobbyUI.java
+++ src/client/ui/LobbyUI.java
@@ -1,7 +1,79 @@
 package client.ui;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by bianca on 06/02/2017.
  */
-public class LobbyUI {
+class LobbyUI extends JPanel {
+
+    private JLabel userLabel;
+    private JTextField userText, clientNames;
+    private JButton loginButton, cancelButton;
+    private JPanel lobby;
+
+    private java.lang.String userName;
+
+    public LobbyUI(JPanel content) {
+
+        this.lobby = content;
+        setOpaque(true);
+
+        userLabel = new JLabel("User name:");
+        add(userLabel);
+
+        userText = new JTextField(20);
+        add(userText);
+
+        loginButton = new JButton("GO");
+        add(loginButton);
+
+        clientNames = new JTextField(userName);
+        add(clientNames, BorderLayout.CENTER);
+
+        cancelButton = new JButton("Cancel");
+        add(cancelButton, BorderLayout.CENTER);
+
+        setLoginVisible();
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setLobbyVisible();
+                clientNames.setText(userText.getText());
+            }
+        });
+
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cardLayout = (CardLayout) lobby.getLayout();
+                cardLayout.next(lobby);
+                setLoginVisible();
+            }
+        });
+
+    }
+
+    private void setLoginVisible() {
+        userLabel.setVisible(true);
+        userText.setVisible(true);
+        loginButton.setVisible(true);
+        userText.setText("");
+
+        clientNames.setVisible(false);
+        cancelButton.setVisible(false);
+    }
+
+    private void setLobbyVisible() {
+        userLabel.setVisible(false);
+        userText.setVisible(false);
+        loginButton.setVisible(false);
+
+        clientNames.setVisible(true);
+        cancelButton.setVisible(true);
+    }
 }
