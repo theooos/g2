9be9da14ab47ee5e60568ffa384523088d4dad2b
Rhys Barrett <rtbarrett27@gmail.com>
2017-03-09 12:14:20 +0000

    Starts building AI Player behaviour trees.

--- src/server/ai/PlayerBrain.java
+++ src/server/ai/PlayerBrain.java
@@ -19,11 +19,24 @@ public class PlayerBrain extends AIBrain {
 
     @Override
     protected void configureBehaviours() {
+        this.flee = new Sequence(intel, this);
+        flee.add(behaviours.getBehaviour("LocateCover"));
+        flee.add(behaviours.getBehaviour("FindPath"));
 
+        this.hunt = new Sequence(intel, this);
+        hunt.add(behaviours.getBehaviour("Wander"));
+        hunt.add(behaviours.getBehaviour("FindPath"));
+        hunt.add(behaviours.getBehaviour("Travel"));
     }
 
     @Override
     public void doSomething() {
+        // Perform checks.
+        boolean hurting = check.doCheck(Check.CheckMode.HEALTH);
+        boolean playerNear = check.doCheck(Check.CheckMode.PROXIMITY);
+
+        // Decide emotion.
+        
 
     }
 
