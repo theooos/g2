5890361fef397b81c04bb222a8248ec50e8d2cc3
Theo Gregory <tjmg@outlook.com>
2017-03-18 16:30:43 +0000

    Have separated out all keyboard/mouse input and polling

--- src/client/graphics/GameManager.java
+++ src/client/graphics/GameManager.java
@@ -17,7 +17,8 @@ public class GameManager {
 
     private GameRenderer gameRenderer;
 
-    private enum Mode {GAME,MENU,SCOREBOARD,GAMEOVER}
+    private enum Mode {GAME, MENU, SCOREBOARD, GAMEOVER}
+
     private Mode mode = Mode.GAME;
 
     private long lastFrame;
@@ -29,11 +30,6 @@ public class GameManager {
     private Connection conn;
     private CollisionManager collisions;
 
-    private boolean fDown;
-    private boolean clickDown;
-    private boolean eDown;
-    private boolean oneDown;
-    private boolean twoDown;
     private boolean healthbar;
     private boolean gameMusic;
     private boolean muted;
@@ -44,17 +40,12 @@ public class GameManager {
         this.gameData = gd;
         this.playerID = playerID;
 
-        fDown = false;
-        clickDown = false;
-        eDown = false;
-        oneDown = false;
-        twoDown = false;
         healthbar = true;
         gameMusic = false;
         muted = true;
 
         collisions = new CollisionManager(gd);
-        gameRenderer = new GameRenderer(gameData,playerID,collisions);
+        gameRenderer = new GameRenderer(gameData, playerID, collisions);
 
         conn.addFunctionEvent("GameOver", this::gameOver);
     }
@@ -64,8 +55,8 @@ public class GameManager {
         mode = Mode.GAMEOVER;
     }
 
-    public void run(){
-        switch (mode){
+    public void run() {
+        switch (mode) {
             case GAME:
                 update();
                 gameRenderer.render();
@@ -78,128 +69,131 @@ public class GameManager {
                 break;
             case GAMEOVER:
                 TextRenderer textRenderer = new TextRenderer();
-                textRenderer.drawText("Game over. To be changed.",0,0);
+                textRenderer.drawText("Game over. To be changed.", 0, 0);
         }
     }
 
     private void update() {
-        gameRenderer.rotation += 1.5f;
-        gameRenderer.rotation %= 360;
         Player me = gameData.getPlayer(playerID);
         checkMusic(me);
+        if (me.isAlive()) makeMovement(me);
+        pollKeyboard(me);
+        pollMouse(me);
 
+        rotatePowerUps();
+        updateFPS(); // update FPS Counter
+    }
+
+    private void rotatePowerUps() {
+        gameRenderer.rotation += 1.5f;
+        gameRenderer.rotation %= 360;
+    }
+
+    private void pollMouse(Player me) {
         if (me.isAlive()) {
-
-            Vector2 pos = me.getPos();
-
-            float xPos = pos.getX();
-            float yPos = pos.getY();
-
-            int delta = getDelta();
-
-            if (Keyboard.isKeyDown(Keyboard.KEY_A)) xPos -= 0.35f * delta;
-            if (Keyboard.isKeyDown(Keyboard.KEY_D)) xPos += 0.35f * delta;
-            if (Keyboard.isKeyDown(Keyboard.KEY_W)) yPos -= 0.35f * delta;
-            if (Keyboard.isKeyDown(Keyboard.KEY_S)) yPos += 0.35f * delta;
-
-            if (Keyboard.isKeyDown(Keyboard.KEY_M)) {
-                if (!muted) {
-                    muted = true;
-                } else {
-                    muted = false;
-                    muteEverything();
-                }
-            }
-
-            if (Keyboard.isKeyDown(Keyboard.KEY_F)) {
-                fDown = true;
-                if (muted) Audio.PHASE.play();
-            } else if (fDown) {
-                fDown = false;
-                int newPhase = 0;
-                if (me.getPhase() == 0) {
-                    newPhase = 1;
-                }
-                Player p = new Player(me);
-                p.setPhase(newPhase);
-                if (collisions.validPosition(p)) {
-                    conn.send(new PhaseObject(me.getID()));
-                    gameRenderer.setPulse(new Pulse(me.getPos(), me.getRadius(), newPhase, 0, 1 - newPhase, 20, 20, newPhase, true));
-                } else {
-                    //invalid phase
-                    gameRenderer.setPulse(new Pulse(me.getPos(), me.getRadius(), 0.3f, 0.3f, 0.3f, 20, 20, me.getPhase(), 250, false));
-                }
-
-            }
-            if (Keyboard.isKeyDown(Keyboard.KEY_E)) {
-                eDown = true;
-            } else if (eDown) {
-                eDown = false;
-                if (me.isWeaponOneOut()) {
-                    conn.send(new SwitchObject(me.getID(), false));
-                } else {
-                    conn.send(new SwitchObject(me.getID(), true));
-                }
-            }
-
-            if (Keyboard.isKeyDown(Keyboard.KEY_1)) {
-                oneDown = true;
-            } else if (oneDown) {
-                oneDown = false;
-                conn.send(new SwitchObject(me.getID(), true));
-            }
-
-            if (Keyboard.isKeyDown(Keyboard.KEY_2)) {
-                twoDown = true;
-            } else if (twoDown) {
-                twoDown = false;
-                conn.send(new SwitchObject(me.getID(), false));
-                gameRenderer.flipDisplayCollisions();
-            }
-
             if (Mouse.isButtonDown(0)) {
-                if (!clickDown) {
-                    conn.send(new FireObject(me.getID(), true));
-                    clickDown = true;
-                }
-            } else if (clickDown) {
+                conn.send(new FireObject(me.getID(), true));
+
                 if (me.activeWeapon() == 1 && muted)
                     Audio.SHOOT2.play();
                 else if (muted) {
                     Audio.SHOOT.play();
                 }
                 conn.send(new FireObject(me.getID(), false));
-                clickDown = false;
             }
+        }
+    }
 
+    private void makeMovement(Player me) {
+        Vector2 pos = me.getPos();
 
-            // keep quad on the screen
-            if (xPos < 0) xPos = 0;
-            if (xPos > 800) xPos = 800;
-            if (yPos < 0) yPos = 0;
-            if (yPos > 600) yPos = 600;
+        float xPos = pos.getX();
+        float yPos = pos.getY();
 
-            if (pos.getX() != xPos || pos.getY() != yPos) {
-                me.setPos(new Vector2(xPos, yPos));
-                if (collisions.validPosition(me)) {
-                    gameData.updatePlayer(me);
-                } else {
-                    me.setPos(pos);
-                }
-            }
+        int delta = getDelta();
 
-            Vector2 tempPos = new Vector2(pos.getX(), ClientSettings.SCREEN_HEIGHT - pos.getY());
-            Vector2 dir = getDirFromMouse(tempPos);
-            if (!me.getDir().equals(dir)) {
-                me.setDir(dir);
-                conn.send(new MoveObject(me.getPos(), me.getDir(), playerID, me.getMoveCount()));
+        if (Keyboard.isKeyDown(Keyboard.KEY_A)) xPos -= 0.35f * delta;
+        if (Keyboard.isKeyDown(Keyboard.KEY_D)) xPos += 0.35f * delta;
+        if (Keyboard.isKeyDown(Keyboard.KEY_W)) yPos -= 0.35f * delta;
+        if (Keyboard.isKeyDown(Keyboard.KEY_S)) yPos += 0.35f * delta;
+
+        // keep quad on the screen
+        if (xPos < 0) xPos = 0;
+        if (xPos > 800) xPos = 800;
+        if (yPos < 0) yPos = 0;
+        if (yPos > 600) yPos = 600;
+
+        if (pos.getX() != xPos || pos.getY() != yPos) {
+            me.setPos(new Vector2(xPos, yPos));
+            if (collisions.validPosition(me)) {
                 gameData.updatePlayer(me);
-            } else if (me.getPos().equals(pos)) {
-                conn.send(new MoveObject(me.getPos(), me.getDir(), playerID, me.getMoveCount()));
+            } else {
+                me.setPos(pos);
             }
         }
 
-        updateFPS(); // update FPS Counter
+        Vector2 tempPos = new Vector2(pos.getX(), ClientSettings.SCREEN_HEIGHT - pos.getY());
+        Vector2 dir = getDirFromMouse(tempPos);
+        if (!me.getDir().equals(dir)) {
+            me.setDir(dir);
+            conn.send(new MoveObject(me.getPos(), me.getDir(), playerID, me.getMoveCount()));
+            gameData.updatePlayer(me);
+        } else if (me.getPos().equals(pos)) {
+            conn.send(new MoveObject(me.getPos(), me.getDir(), playerID, me.getMoveCount()));
+        }
+    }
+
+    private void pollKeyboard(Player me) {
+        while (Keyboard.next()) {
+            // Runs if next key has been PRESSED.
+            if (Keyboard.getEventKeyState()) {
+
+                switch (Keyboard.getEventKey()) {
+
+                    // *** Those that depend on being alive ***
+                    case Keyboard.KEY_E:
+                        if(me.isAlive()) conn.send(new SwitchObject(me.getID(),!me.isWeaponOneOut()));
+                        break;
+
+                    case Keyboard.KEY_F:
+                        if(me.isAlive()) {
+                            if (muted) Audio.PHASE.play();
+                            int newPhase = 0;
+                            if (me.getPhase() == 0) {
+                                newPhase = 1;
+                            }
+                            Player p = new Player(me);
+                            p.setPhase(newPhase);
+                            if (collisions.validPosition(p)) {
+                                conn.send(new PhaseObject(me.getID()));
+                                gameRenderer.setPulse(new Pulse(me.getPos(), me.getRadius(), newPhase, 0, 1 - newPhase, 20, 20, newPhase, true));
+                            } else {
+                                //invalid phase
+                                gameRenderer.setPulse(new Pulse(me.getPos(), me.getRadius(), 0.3f, 0.3f, 0.3f, 20, 20, me.getPhase(), 250, false));
+                            }
+                        }
+                        break;
+
+                    case Keyboard.KEY_1:
+                        if (me.isAlive()) conn.send(new SwitchObject(me.getID(), true));
+                        break;
+
+                    case Keyboard.KEY_2:
+                        if (me.isAlive()) conn.send(new SwitchObject(me.getID(), false));
+                        break;
+
+                    // *** Those that don't depend on being alive ***
+                    case Keyboard.KEY_M:
+                        if (!muted) {
+                            muted = true;
+                        } else {
+                            muted = false;
+                            muteEverything();
+                        }
+                        break;
+                }
+            }
+        }
     }
 
     private Vector2 getDirFromMouse(Vector2 pos) {
