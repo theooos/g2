9e641e33d07d4bed2e473861e0e7f3a023211e19
peran <perantruscott@gmail.com>
2017-03-20 04:32:45 +0000

    fix

--- src/client/graphics/InGameMenuRenderer.java
+++ src/client/graphics/InGameMenuRenderer.java
@@ -1,13 +1,8 @@
 package client.graphics;
 
-import client.graphics.Sprites.BackgroundTexture;
 import client.graphics.Sprites.ISprite;
 import client.graphics.Sprites.InterfaceTexture;
 import objects.GameData;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.util.vector.Vector2f;
-
-import static org.lwjgl.opengl.GL11.*;
 
 /**
  * Renders the in-game menus/scoreboard.
@@ -17,31 +12,13 @@ class InGameMenuRenderer {
     private GameData gameData;
     private int playerID;
 
-    private BackgroundTexture background = new BackgroundTexture(ISprite.OPTIONS);
-    private InterfaceTexture resume;
-    private InterfaceTexture exit;
-    private InterfaceTexture mute;
-
-    private static int PHASE = 0;
-
-    private static Layer options = new Layer();
-
-
     InGameMenuRenderer(GameData gameData, int playerID) {
         this.gameData = gameData;
         this.playerID = playerID;
-        readyMenuLayer();
     }
 
     void readyMenuLayer(){
-        resume = new InterfaceTexture(ISprite.CONTINUE);
-        exit = new InterfaceTexture(ISprite.ENDGAME);
-        mute = new InterfaceTexture(ISprite.MUTE);
-
-        background.spawn(0,new Vector2f(400f,300f),PHASE,options);
-        resume.spawn(2, new Vector2f(400f, 270f), PHASE, options);
-        exit.spawn(3, new Vector2f(400f, 180f), PHASE, options);
-        mute.spawn(4, new Vector2f(400f, 90f), PHASE, options);
+        InterfaceTexture background = new InterfaceTexture(ISprite.BACKGROUND);
     }
 
     void handleClickedMenu(){
@@ -57,8 +34,8 @@ class InGameMenuRenderer {
     }
 
     void renderMenu(){
-       enableTextureScreen();
-       options.render(PHASE);
+        TextRenderer textRenderer = new TextRenderer();
+        textRenderer.drawText("The in-game menu will be here.", 0, 50);
     }
 
     void renderScoreboard(){
@@ -69,13 +46,5 @@ class InGameMenuRenderer {
     void renderEndScreen(){
         TextRenderer textRenderer = new TextRenderer();
         textRenderer.drawText("The end game screen will be here.", 0, 50);
-    }
-
-    private void enableTextureScreen() {
-        GL11.glClearColor( 0.0f, 0.0f, 1.0f, 0.0f );
-        GL11.glEnable(GL_BLEND);
-        GL11.glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
-        GL11.glEnable(GL_TEXTURE_2D);
-        GL11.glDisable(GL_DEPTH_TEST);
     }
 }
